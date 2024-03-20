de4b2b5e9c21dc376494409072c91c725f1cd920
Congthang1707 <congthangnguyen1707@gmail.com>
2024-03-20 11:19:31 +0700

    First commit

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import Welcome from "./home/Welcome";
+import ProductList from "./products/ProductList";
+import ProductRow from "./products/ProductRow";
+import ProductCardView from "./products/ProductCardView";
+import BackBtn from "./BackBtn";
+import Button from "./Button";
+
+export { 
+    Welcome,
+    ProductList,
+    ProductRow,
+    ProductCardView,
+    BackBtn,
+    Button
+} 
